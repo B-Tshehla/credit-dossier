3757ba0998ff69b17dcac01aa5be361ac0914ff4
B-Tshehla <Boitumelotshehla@gmail.com>
2022-11-27 10:33:57 +0200

    email models/dossier/created EmailMessage file and Theme file

---
+++ src/main/java/com/enfint/dossier/dto/EmailMessage.java
@@ -0,0 +1,17 @@
+package com.enfint.dossier.dto;
+
+import com.enfint.dossier.dataEnum.Theme;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailMessage {
+    String address;
+    Theme theme;
+    Long applicationId;
+}
